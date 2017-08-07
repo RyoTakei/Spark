07da8b9ada17d478ab28f0d1de00179beeff41cc
RyoTakei <takeiryou926@gmail.com>
2017-08-06 23:54:53 -0700

    updates

---
+++ DayOne/lists.py
@@ -0,0 +1,8 @@
+listOne = ["juice", "Tomatos", "Potatos", "Bananas"]
+
+firstItem = listOne[0]
+print("The first item is", firstItem)
+
+listOne[0] = "Green Juice"
+
+print("The First item is now", listOne[0])
