e190f66bf35d48cbb20016c71868db02b90bcfa0
RyoTakei <takeiryou926@gmail.com>
2017-08-06 23:44:32 -0700

    updates

---
+++ DayOne/comments.py
@@ -0,0 +1,15 @@
+#this is a single comment
+
+'''
+this is mutli lines comments
+
+
+
+haha
+
+2898 is better
+
+SST is better
+
+ewwwwww Westview
+'''
---
+++ DayOne/print.py
@@ -0,0 +1,10 @@
+print()
+
+print(4)
+
+print("Hey!!")
+
+print(4.5642)
+
+school = "SST"
+print(school)
