4b1838e22e2727fd604d26f8215271bf7689396e
RyoTakei <takeiryou926@gmail.com>
2017-08-07 22:21:38 -0700

    Updates

---
+++ DayTwo/conditional.py
@@ -0,0 +1,39 @@
+age = 12
+
+
+if age > 16:
+    print("You can drive")
+else:
+    print("You cannot drive")
+
+if age >= 21:
+    print("You can drink")
+else:
+    print("You cannot drink")
+
+
+######################
+
+if age > 18:
+    print("You are an adult")
+elif age >= 13:
+    print("You are a teen")
+else:
+    print("You are a kid")
+
+#####################
+
+print("\nTrue & True =", True & True)
+print("\nTrue & False =", True & False)
+print("\nFalse & False =", False & False)
+print("\nTrue | True =", True | True)
+print("\nTrue | False =", True | False)
+print("\nFalse | False =", False | False)
+print("\n!True =", not True)
+print("\n!False =", not False)
+
+
+####################
+
+
+
