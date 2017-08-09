b3120d29159eb4e843cdc25486bf97b2363662fe
RyoTakei <takeiryou926@gmail.com>
2017-08-09 16:30:10 -0700

    Updates

--- DayOne/math.py
+++ DayOne/math.py
@@ -11,3 +11,26 @@ print("5 % 2 =", 5 % 2)
 print("5 ** 2 =", 5 ** 2)
 
 print("5 // 2 =", 5 // 2)
+print()
+
+#########################
+
+count1 = 1
+
+print(count1)
+
+count1 += 1
+
+print(count1)
+print()
+
+#########################
+
+count2 = 5
+
+print(count2)
+
+count2 -= 1
+
+print(count2)
+print()
--- DayOne/veriables.py
+++ DayOne/veriables.py
@@ -3,3 +3,18 @@ var2 = "Hello"
 var3 = 5.231232324
 var4 = "HAHAHAHHAHAHAHAH"
 var5 = ["annika", "Ryo", "Micah", "Solomon"]
+var6 = True
+
+##############################
+
+# variable should not start with numbers
+'''
+two words variable should look something like this....
+
+myCat = "name"
+firstNumber = 6
+moneyYouSpent = 45
+isItDone = False
+'''
+
+
