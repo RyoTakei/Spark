8c85f6038602ec8035944bda3e0045c02793f705
RyoTakei <takeiryou926@gmail.com>
2017-08-25 10:30:41 -0700

    Updates

--- DayFour/pygame2.py
+++ DayFour/pygame2.py
@@ -22,18 +22,10 @@ def display_image(image, x, y):
 def main():
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pyfgame.QUIT:
                 return
 
-        x = random.randint(0, display_width)
-        y = random.randint(0, display_height)
-
-        display_image(linux, x, y)
-        time.sleep(1)
-
-        pygame.display.update()
-        clock.tick(60)
-
+            print(event)
 
 if __name__ == '__main__':
     main()
--- DayFour/student_pygame.py
+++ DayFour/student_pygame.py
@@ -1,47 +1,70 @@
+# it imports pygame
 import pygame
 
 pygame.init()
 
-display_height = 800
-display_width = 800
+# setting the size of display/window
+display_height = 500
+display_width = 500
 
-
+# creating the display/window. size in tuple
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 
 clock = pygame.time.Clock()
 
-
+# loading a image
 images = pygame.image.load("linux.png")
+# setting the size. size in tuple
 images = pygame.transform.scale(images, (60, 60))
 
-# object_changeValue = 5
 linux_changeValue = 5
 
 
 def image(x, y, image):
+    '''
+    This function displays a image
+    :param x: x pos of the image
+    :param y: y pos of the image
+    :param image: image you want to display
+    :return: nothing
+    '''
     gameDisplay.blit(image, (x, y))
 
 
+# this is the main function
 def main():
-    x = 200
-    y = 20
+    # set the x and y pos of the image
+    x = 200 # x = display_width / 2
+    y = 20  # y = display_height / 2
 
     x_change = 0
     y_change = 0
 
     while True:
+        # for every event that happens during the game.....
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                # it exits out
                 return
 
+            # any keys pressed???
             if event.type == pygame.KEYDOWN:
+
+                # is the key left arrow??
                 if event.key == pygame.K_LEFT:
-                    x_change = -linux_changeValue
+                    # changing the x value by -5 but not y
+                    x_change = - linux_changeValue
                     y_change = 0
+
+                # is the key right arrow??
                 elif event.key == pygame.K_RIGHT:
+                    # changing the x value by 5 but not y
                     x_change = linux_changeValue
                     y_change = 0
+
+                # is the key up arrow??
                 elif event.key == pygame.K_UP:
+                    # changing the y value by 5 but not x
                     y_change = -linux_changeValue
                     x_change = 0
                 elif event.key == pygame.K_DOWN:
